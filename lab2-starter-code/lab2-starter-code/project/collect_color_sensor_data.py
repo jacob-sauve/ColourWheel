c16104dd35db04d8d5a75335ff1f69375489a5a0
IvanGaspart <ivan.torrefacteuramical@gmail.com>
2026-01-28 12:21:57 -0500

    color data collection code finished v1

--- lab2-starter-code/lab2-starter-code/project/collect_color_sensor_data.py
+++ lab2-starter-code/lab2-starter-code/project/collect_color_sensor_data.py
@@ -6,22 +6,34 @@ It must be run on the robot.
 """
 
 # Add your imports here, if any
-from utils.brick import EV3ColorSensor, wait_ready_sensors, TouchSensor
+from utils.brick import EV3ColorSensor, wait_ready_sensors, TouchSensor, reset_brick
 
 
 COLOR_SENSOR_DATA_FILE = "../data_analysis/color_sensor.csv"
 
 # complete this based on your hardware setup
-_ = EV3ColorSensor(...)
-_ = TouchSensor(...)
+COLOR_SENSOR = EV3ColorSensor(2)
+TOUCH_SENSOR = TouchSensor(1)
 
 wait_ready_sensors(True) # Input True to see what the robot is trying to initialize! False to be silent.
 
 
 def collect_color_sensor_data():
-    "Collect color sensor data."
-    ...
-
+    try:
+        while True:
+            if TouchSensor(1).is_pressed:
+                print("Touch Sensor Pressed")
+                color_data = COLOR_SENSOR.get_rgb()
+                if color_data is not None:
+                    print(COLOR_SENSOR.get_rgb())
+                    with open(COLOR_SENSOR_DATA_FILE, "w") as output_file:
+                        output_file.write(f"{color_data}\n")
+    except BaseException:
+        pass
+    finally:
+        print("Done collecting color sensor data")
+        reset_brick()
+        exit()
 
 if __name__ == "__main__":
     collect_color_sensor_data()
