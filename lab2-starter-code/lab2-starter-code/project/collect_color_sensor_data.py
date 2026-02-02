e84b45cf2ad92f7ea3431324ef7ca7a065ddf1dd
jacob-sauve <woozy-army-trivial@duck.com>
2026-02-02 18:38:46 +0000

    added data and code

--- lab2-starter-code/lab2-starter-code/project/collect_color_sensor_data.py
+++ lab2-starter-code/lab2-starter-code/project/collect_color_sensor_data.py
@@ -6,32 +6,54 @@ It must be run on the robot.
 """
 
 # Add your imports here, if any
-from utils.brick import EV3ColorSensor, wait_ready_sensors, TouchSensor, reset_brick
+import time
+from utils.brick import BP, EV3ColorSensor, wait_ready_sensors, TouchSensor, reset_brick
 
 
 COLOR_SENSOR_DATA_FILE = "../data_analysis/color_sensor.csv"
 
 # complete this based on your hardware setup
-COLOR_SENSOR = EV3ColorSensor(2)
+COLOR_SENSOR = EV3ColorSensor(3)
 TOUCH_SENSOR = TouchSensor(1)
+
+#COLOR_SENSOR.set_mode("rgb")
 
 wait_ready_sensors(True) # Input True to see what the robot is trying to initialize! False to be silent.
 
-
 def collect_color_sensor_data():
     try:
+        measured = False # flag to only measure once per click
+        output_file = open(COLOR_SENSOR_DATA_FILE, "w")
         while True:
-            if TouchSensor(1).is_pressed:
-                print("Touch Sensor Pressed")
-                color_data = COLOR_SENSOR.get_rgb()
-                if color_data is not None:
-                    print(COLOR_SENSOR.get_rgb())
-                    with open(COLOR_SENSOR_DATA_FILE, "w") as output_file:
+            is_pressed = TOUCH_SENSOR.is_pressed()
+            color_data = COLOR_SENSOR.get_rgb()
+            if is_pressed:
+                if not measured:
+                    print("touch sensor pressed")
+                    if color_data is not None:
+                        print(color_data)
                         output_file.write(f"{color_data}\n")
-    except BaseException:
-        pass
+                    measured = True
+            else:
+                measured = False
+            time.sleep(0.1)
+#                if not measured:
+#                    measured = True
+#                    print("Touch Sensor Pressed")
+#                    color_data = COLOR_SENSOR.get_rgb()
+#                    if None not in color_data:
+#                        print(color_data)
+#                        output_file.write(f"{color_data}\n")
+#                        with open(COLOR_SENSOR_DATA_FILE, "a") as output_file:
+#                            output_file.write(f"{color_data}\n")
+#                            output_file.close()
+#            else:
+#                measured = False
+    except Exception as e:
+        print("Error: ", e)
     finally:
         print("Done collecting color sensor data")
+        output_file.close()
         reset_brick()
         exit()
 
