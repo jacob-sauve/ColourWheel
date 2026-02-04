6264d6f76da27094b75dc5a116f84a9172389a6c
jacob-sauve <woozy-army-trivial@duck.com>
2026-02-03 20:20:11 -0500

    redocumented color_sensor_data_analysis.py

--- source/project/collect_color_sensor_data.py
+++ source/project/collect_color_sensor_data.py
@@ -5,18 +5,16 @@ This test is used to collect data from the color sensor.
 It must be run on the robot.
 """
 
-# Add your imports here, if any
+# imported modules
 import time
 from utils.brick import BP, EV3ColorSensor, wait_ready_sensors, TouchSensor, reset_brick
 
-
+# constants
 COLOR_SENSOR_DATA_FILE = "../data_analysis/color_sensor.csv"
 
 # complete this based on your hardware setup
 COLOR_SENSOR = EV3ColorSensor(3)
 TOUCH_SENSOR = TouchSensor(1)
-
-#COLOR_SENSOR.set_mode("rgb")
 
 wait_ready_sensors(True) # Input True to see what the robot is trying to initialize! False to be silent.
 
@@ -29,26 +27,17 @@ def collect_color_sensor_data():
             color_data = COLOR_SENSOR.get_rgb()
             if is_pressed:
                 if not measured:
+                    # only register click if not already done for this
+                    # button-press
                     print("touch sensor pressed")
                     if color_data is not None:
                         print(color_data)
                         output_file.write(f"{color_data}\n")
                     measured = True
             else:
+                # reset flag
                 measured = False
             time.sleep(0.1)
-#                if not measured:
-#                    measured = True
-#                    print("Touch Sensor Pressed")
-#                    color_data = COLOR_SENSOR.get_rgb()
-#                    if None not in color_data:
-#                        print(color_data)
-#                        output_file.write(f"{color_data}\n")
-#                        with open(COLOR_SENSOR_DATA_FILE, "a") as output_file:
-#                            output_file.write(f"{color_data}\n")
-#                            output_file.close()
-#            else:
-#                measured = False
     except Exception as e:
         print("Error: ", e)
     finally:
--- source/project/color_sensor_data_analysis.py
+++ source/project/color_sensor_data_analysis.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 def analyse_pseudo_csv(filepath):
-    """analyse colour sensor output from file at filepath"""
+    """Analyse colour sensor output from file at filepath and return channel mean and SD in dict form"""
     data = list()
     output = dict()
     with open(filepath, 'r') as file:
@@ -16,7 +16,7 @@ def analyse_pseudo_csv(filepath):
             sum_rgb = r+g+b
             # append normalised values
             data.append([r/sum_rgb, g/sum_rgb, b/sum_rgb])
-    mean = np.mean(data, axis=0) # mean along columns <=> colors
+    mean = np.mean(data, axis=0) # mean along columns <=> color channels
     std_dev = np.std(data, axis=0)
     for i,color in enumerate(("R","G","B")):
         output[color] = {
@@ -27,4 +27,5 @@ def analyse_pseudo_csv(filepath):
 
 
 if __name__ == "__main__":
+    # run with input filepath as command line argument
     print(analyse_pseudo_csv(sys.argv[1]))
