d1d86c73b9d9de38cce4233e7fea0f2889ad78cb
jacob-sauve <woozy-army-trivial@duck.com>
2026-02-11 17:52:34 +0000

    semi-working color classification script

---
+++ source/project/classtest.py
@@ -0,0 +1,14 @@
+import math
+def classify(rgb):
+    mag = math.sqrt(rgb[0]**2 + rgb[1]**2 + rgb[2]**2)
+    for i in range(3):
+        rgb[i] = rgb[i] / mag
+    omega = math.atan(math.asin(rgb[2]) / math.atan(rgb[0] / rgb[1]))
+    color = ""
+    if (omega <= 0.3):
+        color = "red"
+    elif (omega <= 0.8):
+        color = "purple"
+    elif (omega <= 1.2):
+        color = "blue"
+    return color
---
+++ source/project/omega_collection.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+"""
+This test is used to collect data from the color sensor.
+It must be run on the robot.
+"""
+
+# imported modules
+import time
+from utils.brick import BP, EV3ColorSensor, wait_ready_sensors, TouchSensor, reset_brick
+from classtest import classify
+
+# constants
+COLOR_SENSOR_DATA_FILE = "../data_analysis/omega_values.csv"
+
+# complete this based on your hardware setup
+COLOR_SENSOR = EV3ColorSensor(2)
+TOUCH_SENSOR = TouchSensor(1)
+
+wait_ready_sensors(True) # Input True to see what the robot is trying to initialize! False to be silent.
+
+def collect_color_sensor_data():
+    try:
+        measured = False # flag to only measure once per click
+        output_file = open(COLOR_SENSOR_DATA_FILE, "w")
+        while True:
+            is_pressed = TOUCH_SENSOR.is_pressed()
+            color_data = COLOR_SENSOR.get_rgb()
+            if is_pressed:
+                if not measured:
+                    # only register click if not already done for this
+                    # button-press
+                    print("touch sensor pressed")
+                    if color_data is not None:
+                        try:
+                            print(classify(color_data))
+                            output_file.write(f"{classify(color_data)}\n")
+                        except:
+                            print("Invalid reading - try again")
+                    else:
+                        print("Invalid reading - try again")
+                    measured = True
+            else:
+                # reset flag
+                measured = False
+            time.sleep(0.1)
+    except Exception as e:
+        # print error message for debugging
+        print("Error: ", e)
+    finally:
+        print("Done collecting color sensor data")
+        # close file for memory safety
+        output_file.close()
+        # reset brick and exit
+        reset_brick()
+        exit()
+
+if __name__ == "__main__":
+    collect_color_sensor_data()
