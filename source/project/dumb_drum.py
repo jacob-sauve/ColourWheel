239b91f29ab4f16bf9fc6a3d4f84d6b5b011e18c
Jacob Sauvé <woozy-army-trivial@duck.com>
2026-02-12 16:14:06 -0500

    made safer with finally and quadrupling emergency stop poll frequency

--- source/project/dumb_drum.py
+++ source/project/dumb_drum.py
@@ -2,26 +2,28 @@ from utils.brick import Motor, TouchSensor, wait_ready_sensors, reset_brick
 import time
 from setup_brickpi import setup_ports
 
-STOP, motor = setup_ports(emergency_stop=True, drum_motor=True)
+INSTRUCTION_BUFFER = 0.2    # seconds, nonzero so motor doesn't die
 
-# Designates to Encoder, that the current physical position is 0 degrees
-motor.reset_encoder()
+if __name__ == "__main__":
+    try:
+        # setup ports
+        stop, motor = setup_ports(emergency_stop=True, drum_motor=True)
+        
+        # Designates to Encoder, that the current physical position is 0 degrees
+        motor.reset_encoder()
+        
+        # Prevents position control from going over either:
+        # 80% power or 360 deg/sec, whichever is slower
+        motor.set_limits(power=80, dps=360)
 
-# Prevents position control from going over either:
-# 50% power or 90 deg/sec, whichever is slower
-motor.set_limits(power=80, dps=360)
-
-#motor.set_limits() # UNLIMITED POWER (AND SPEED)
-
-# Will rotate 10 degrees backwards from current position.
-# Does not care about the absolute position.
-while not STOP.is_pressed():
-    motor.set_position_relative(90)
-    time.sleep(0.4)
-    motor.set_position_relative(-90)
-    time.sleep(0.4)
-
-print("Stopped")
-reset_brick()
-exit()
-
+        direction = +1
+        # run until emergency stop is actuated
+        while not stop.is_pressed():
+            # added direction to have emergency stop verification twice as frequently
+            motor.set_position_relative(direction * 90)
+            time.sleep(INSTRUCTION_BUFFER)
+            direction *= -1
+    finally:
+        print("Stopped")
+        reset_brick()
+        exit()
