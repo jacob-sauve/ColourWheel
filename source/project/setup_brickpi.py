39479d8b29fe39290ab16e9bd2d64a9a9d34e06b
Jacob Sauvé <woozy-army-trivial@duck.com>
2026-02-12 16:41:37 -0500

    added dictionary to ensure mapping modularity and concision

--- source/project/setup_brickpi.py
+++ source/project/setup_brickpi.py
@@ -1,18 +1,48 @@
 #!/usr/bin/env python3
 
 """
-Setup the BrickPi sensors & actuator
+Standard setup for the BrickPi sensors & actuator
 """
 
 # import modules
 from utils.brick import TouchSensor, wait_ready_sensors, EV3UltrasonicSensor, EV3ColorSensor, Motor
 
 # STANDARD PORTS FOR ALL TESTS + MAIN CODE
+# vvv (change here if needed) vvv
 PLAY_BUTTON = 1         # for flute actuation
 COLOR_SENSOR = 2
 EMERGENCY_STOP = 3
 US_SENSOR = 4           # drum button
 DRUM_MOTOR = "D"        # drum motor 
+
+# master dictionary
+port_map = {
+    "play_button" : {
+        "label" : "play button (touch)",
+        "port" : PLAY_BUTTON,
+        "function" : TouchSensor
+    },
+    "color_sensor" : {
+        "label" : "color sensor",
+        "port" : COLOR_SENSOR,
+        "function" : EV3ColorSensor
+    },
+    "emergency_stop" : {
+        "label" : "emergency stop (touch)",
+        "port" : EMERGENCY_STOP,
+        "function" : TouchSensor
+    },
+    "us_sensor" : {
+        "label" : "drum button (US)",
+        "port" : US_SENSOR,
+        "function" : EV3UltrasonicSensor
+    },
+    "drum_motor" : {
+        "label" : "drum motor",
+        "port" : DRUM_MOTOR, 
+        "function" : Motor
+    },
+}
 
 
 def setup_ports(play_button=False, color_sensor=False, emergency_stop=False, us_sensor=False, drum_motor=False, verbose=True):
@@ -23,25 +53,11 @@ def setup_ports(play_button=False, color_sensor=False, emergency_stop=False, us_
     ports = list()
     if verbose:
         print("Connect the following:")
-    if play_button:
-        if verbose:
-            print(f"\t*play button (touch) to port S{PLAY_BUTTON}")
-        ports.append(TouchSensor(PLAY_BUTTON))
-    if color_sensor:
-        if verbose:
-            print(f"\t*color sensor to port S{COLOR_SENSOR}")
-        ports.append(EV3ColorSensor(COLOR_SENSOR))
-    if emergency_stop:
-        if verbose:
-            print(f"\t*emergency stop (touch) to port S{EMERGENCY_STOP}")
-        ports.append(TouchSensor(EMERGENCY_STOP))
-    if us_sensor:
-        if verbose:
-            print(f"\t*drum button (US) to port S{US_SENSOR}")
-        ports.append(EV3UltrasonicSensor(US_SENSOR))
-    if drum_motor:
-        if verbose:
-            print(f"\t*drum motor to port M{DRUM_MOTOR}")
-        ports.append(Motor(DRUM_MOTOR))
-    wait_ready_sensors(True)
+    for varname, flag in locals():
+        if varname != "verbose":
+            if flag:
+                if verbose:
+                    print(f"\t*{port_map[varname]["label"]} to port S{port_map[varname]["port"]}")
+                ports.append(port_map[varname]["function"](port_map[varname]["port"]))
+            wait_ready_sensors(True)
     return ports
