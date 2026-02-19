ede6bf4c89545c6a539f92f1bfabe9206d059872
Jacob Sauvé <woozy-army-trivial@duck.com>
2026-02-19 18:14:22 -0500

    redocumented omega_collection

--- source/project/omega_collection.py
+++ source/project/omega_collection.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 """
-This test is used to collect hue-adjacent values ("omega" values) for a brick.
+Test code used to collect hue-adjacent values ("omega" values) for a brick.
+v 1.1
+2026-02-19
 """
 
 # imported modules
@@ -10,13 +12,18 @@ from utils.brick import BP, EV3ColorSensor, TouchSensor, reset_brick
 from classtest import classify
 
 # constants
-COLOR_SENSOR_DATA_FILE = "../data_analysis/omega_values.csv"
+OUTPUT_DATA_FILE = "../data_analysis/omega_values.csv"
 
 
 def collect_brick_omega_values(color_sensor, touch_sensor):
+    """Classify color sensor input data using classtest.classify(). Outputs to file.
+    Keyword arguments:
+        color_sensor    -- the EV3ColorSensor object polled for R,G,B values
+        touch_sensor    -- the TouchSensor object used as a trigger for color sensor polling
+    """
     try:
         measured = False # flag to only measure once per click
-        output_file = open(COLOR_SENSOR_DATA_FILE, "w")
+        output_file = open(OUTPUT_DATA_FILE, "w")
         while True:
             is_pressed = touch_sensor.is_pressed()
             color_data = color_sensor.get_rgb()
@@ -27,8 +34,10 @@ def collect_brick_omega_values(color_sensor, touch_sensor):
                     print("touch sensor pressed")
                     if color_data is not None:
                         try:
-                            print(classify(color_data))
-                            output_file.write(f"{classify(color_data)}\n")
+                            # classify color
+                            classification = classify(color_data)
+                            print(classification)
+                            output_file.write(f"{classification}\n")
                         except:
                             print("Invalid reading - try again")
                     else:
@@ -40,7 +49,7 @@ def collect_brick_omega_values(color_sensor, touch_sensor):
             time.sleep(0.1)
     except Exception as e:
         # print error message for debugging
-        print("Error: ", e)
+        print("Error:", e)
     finally:
         print("Done collecting color sensor data")
         # close file for memory safety
@@ -51,5 +60,6 @@ def collect_brick_omega_values(color_sensor, touch_sensor):
 
 if __name__ == "__main__":
     from setup_brickpi import setup_ports
+    
     TOUCH_SENSOR, COLOR_SENSOR = setup_ports(play_button=True, color_sensor=True)
     collect_brick_omega_values(COLOR_SENSOR, TOUCH_SENSOR)
